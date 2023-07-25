c2848974786ec0bdf635f09dbee932d8251224ef
Ifedolapoomoniyi <ifedolapo0001@gmail.com>
2023-07-25 01:40:56 +0100

    feat: code update:

--- foodapp/src/components/Dashboard.jsx
+++ foodapp/src/components/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/Dashboard.css'
 import ImageComp from './ImageComp'
 import img4 from '../imgs/img4.png'
@@ -6,12 +6,24 @@ import img2 from '../imgs/img2.png'
 import img3 from '../imgs/img3.png'
 import img1 from '../imgs/img1.png'
 import img5 from '../imgs/img5.png'
+import ModalComp from './ModalComp'
 
 const Dashboard = () => {
+
+    const [openModal, setopenModal] = useState(false)
+
+    const handleOpen = ()=>{
+        setopenModal(true)
+    }
+    const handleClose = ()=>{
+        setopenModal(false)
+    }
+
+
   return (
     <div className="Dashboard">
         <div className="dashboard-header">
-            <h3>Good morning, Ifedolapo!</h3>
+            <h3>Good morning, {localStorage.getItem("name")}!</h3>
             <img src="" alt="Profile Image" />
         </div>
         <div className="dashboard-body">
@@ -20,7 +32,10 @@ const Dashboard = () => {
                 name={'Stir Fry Pasta'}
                 text={'The in-house pasta and chicken by chef Moose'}
                 image={img4}
-                />
+                >   
+                    <ModalComp open = {openModal} handleClose = {handleClose}><div>Can this modal take some stuff from here?</div></ModalComp>
+                    <button onClick={handleOpen}>Open MOdal</button>
+                </ImageComp>
             </div>
             <div className="dashboard-image">
                 <ImageComp 
--- foodapp/src/components/ImageComp.jsx
+++ foodapp/src/components/ImageComp.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import '../styles/ImageComp.css'
 
-const ImageComp = (props) => {
+const ImageComp = (props, ...children) => {
     const body = props.body
     const head = props.head
   return (
@@ -9,7 +9,8 @@ const ImageComp = (props) => {
         <img src={props.image} alt="" />
         <h3 style={{color: head}}>{props.name}</h3>
         <p style={{color: body}}>{props.text}</p>
-
+        {props.children}
+        
     </div>
   )
 }
