53640a926bc43532fff9e2295ca77d43a2affde5
Ifedolapoomoniyi <ifedolapo0001@gmail.com>
2023-07-28 05:30:41 +0100

    data store

---
+++ foodapp/src/data/data.js
@@ -0,0 +1,46 @@
+export const Food = [
+    {
+        name: "Burger",
+        description: "A hot burger made from the rarest of meats",
+        price: 1000,
+        img: "img4",
+        cart: true,
+        about: 'Just have a single bite of this Black Forest pastry and it will all make a proper sense to you. The kick of cherry and rich chocolate of this super light, airy pastry will definitely make you feel "wow". The perfect combination of cherry cream and rich chocolate can provide the ultimate fulfillment to your dessert craving.',
+    },
+
+    {
+        name: "StirFry Spaghetti",
+        description: "In House Spaghetti made by our best chef",
+        price: 3000,
+        img: "img1",
+        cart: true,
+        about: 'Just have a single bite of this Black Forest pastry and it will all make a proper sense to you. The kick of cherry and rich chocolate of this super light, airy pastry will definitely make you feel "wow". The perfect combination of cherry cream and rich chocolate can provide the ultimate fulfillment to your dessert craving.',
+    },
+
+    {
+        name: "MeatBalls",
+        description: "Who doesn't love meat with some balls",
+        price: 5000,
+        img: "img4",
+        cart: true,
+        about: 'Just have a single bite of this Black Forest pastry and it will all make a proper sense to you. The kick of cherry and rich chocolate of this super light, airy pastry will definitely make you feel "wow". The perfect combination of cherry cream and rich chocolate can provide the ultimate fulfillment to your dessert craving.',
+    },
+
+    {
+        name: "Daiiy special",
+        description: "The day's special meal, in a delicious package",
+        price: 4000,
+        img: "img2",
+        cart: true,
+        about: 'Just have a single bite of this Black Forest pastry and it will all make a proper sense to you. The kick of cherry and rich chocolate of this super light, airy pastry will definitely make you feel "wow". The perfect combination of cherry cream and rich chocolate can provide the ultimate fulfillment to your dessert craving.',
+    },
+
+    {
+        name: "Banana Delicacy",
+        description: "Bananas wrapped in royal goodness",
+        price: 1500,
+        img: "img2",
+        cart: true,
+        about: 'Just have a single bite of this Black Forest pastry and it will all make a proper sense to you. The kick of cherry and rich chocolate of this super light, airy pastry will definitely make you feel "wow". The perfect combination of cherry cream and rich chocolate can provide the ultimate fulfillment to your dessert craving.',
+    }
+]
