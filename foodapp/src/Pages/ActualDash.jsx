f5e11752a2fc2f501245a7d3187c90dee2277f52
Ifedolapoomoniyi <ifedolapo0001@gmail.com>
2023-07-31 04:12:58 +0100

    added Toasts(alerts)

--- foodapp/src/Pages/ActualDash.jsx
+++ foodapp/src/Pages/ActualDash.jsx
@@ -1,14 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Sidebar from '../components/Sidebar'
 import Dashboard from '../components/Dashboard'
 import '../styles/ActualDash.css'
 import ModalComp from '../components/ModalComp'
+import { cartStore } from '../components/ImageComp'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
 
 const ActualDash = () => {
+  const cartLength = cartStore.length
+  let ignore = false;
+
+  useEffect(() => {
+
+    if (!ignore) {
+    toast.success("Welcome to Lillies")
+  }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  return () => {ignore = true}
+  }, [])    
+
+
+
   return (
     <div className="ActualDash">
-        <Sidebar/>
-        <Dashboard/>
+        <ToastContainer/>
+        <Sidebar cartLength={cartLength}/>
+        <Dashboard />
         <ModalComp/>
     </div>
   )
