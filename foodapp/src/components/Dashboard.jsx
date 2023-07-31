17f0a7678f84d0692929f73c3a549d0bce9145ec
Ifedolapoomoniyi <ifedolapo0001@gmail.com>
2023-07-31 03:59:10 +0100

    added avatar

--- foodapp/src/components/Dashboard.jsx
+++ foodapp/src/components/Dashboard.jsx
@@ -1,15 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
 import "../styles/Dashboard.css";
 import ImageComp from "./ImageComp";
 import { Food } from "../data/data";
+import Avatar from "../imgs/Avatar.jpg"
 
 const Dashboard = () => {
 
 	return (
 		<div className="Dashboard">
 			<div className="dashboard-header">
-				<h3>Good morning, {localStorage.getItem("name")}!</h3>
-				<img src="" alt="Profile" />
+				<div className="dashboard-header-welcome">
+					<h3>Good morning, {localStorage.getItem("name")}!</h3>
+					<div>What delicious meal are you craving today?</div>
+
+				</div>
+				<img src={Avatar} alt="Profile" />
 			</div>
 			<div className="dashboard-body">
 					{Food.map((item, key) => {
