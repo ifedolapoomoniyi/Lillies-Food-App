a1e8045dc86db5f7ecd900a2dd163ce35859d702
Ifedolapoomoniyi <ifedolapo0001@gmail.com>
2023-08-14 23:56:43 +0100

    Mobile responsiveness

--- foodapp/src/components/Navbar.jsx
+++ foodapp/src/components/Navbar.jsx
@@ -1,15 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useState } from 'react';
 import '../styles/Navbar.css'
 import logo from '../imgs/logo.png';
 import { Link } from "react-router-dom"
 
 const Navbar = () => {
+
+
+  useEffect(() => {
+    const navbar = document.querySelector('.Navbar');
+    const hamburger = document.querySelector('.hamburger');
+
+      hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('active');
+        navbar.classList.toggle('active');
+        console.log("working")
+      })
+    }, []
+  )
+
+
   return (
-    <div className="Navbar">
-        <div className="company-name">
-            <span><img src={logo} alt="logo" className='company-logo'/></span>
-            <span>Lilies</span>
-        </div>
+    <>
+    <div className="company-name">
+      <span><img src={logo} alt="logo" className='company-logo'/></span>
+      <span>Lilies</span>
+    </div>
+    <div className="Navbar active">
         <div className="Navbar-links">
             <span ><Link to={'/'} className="links">Home</Link></span>
             <span ><Link to={"/login"} className="links">Login</Link></span>
@@ -17,6 +34,15 @@ const Navbar = () => {
 
         </div>
     </div>
+
+    <div className="hamburger">
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+    </div>
+
+    </>
+    
   )
 }
 
